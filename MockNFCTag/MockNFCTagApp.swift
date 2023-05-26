0b338e7c8eaad023b4402c22ed12c6cd696363c7
Jose <jose.aponte@jappsku.co>
2023-05-26 16:35:48 -0500

    Initial Commit

---
+++ MockNFCTag/MockNFCTagApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockNFCTagApp.swift
+//  MockNFCTag
+//
+//  Created by Jose Aponte on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MockNFCTagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
